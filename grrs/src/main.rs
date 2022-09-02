fda0cb3a1dc9ec636a3fcc8ff2fe657a56bc6e75
Kelvin <quocvm1@gmail.com>
2022-09-02 23:29:04 +0800

    [simpleCLI] using BufReader instead of 'read_to_string' for better performance

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
 use clap::Parser;
+use std::io::{self, BufRead};
+use std::fs::File;
+use std::path::Path;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -14,7 +17,7 @@ struct Cli {
 
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
     // let pattern = std::env::args().nth(1).expect("no pattern given");
     // let path = std::env::args().nth(2).expect("no path given");
@@ -26,14 +29,30 @@ fn main() {
 
     let args = Cli::parse();
     //[improvement] use BufReader instead 
-    let content = std::fs::read_to_string(args.path)
-        .expect("cound not read file");
-
-    // how to know what are the menthods available in 'content'
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    // let content = std::fs::read_to_string(args.path)
+    //     .expect("cound not read file");
+    // // how to know what are the menthods available in 'content'
+    // for line in content.lines() {
+    //     if line.contains(&args.pattern) {
+    //         println!("{}", line);
+    //     }
+    // }
+    if let Ok(lines) = read_lines(args.path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(line) = line {
+                // todo!("need to improve nest 'if' statment");
+                if line.contains(&args.pattern) {
+                    println!("{}", line);
+                }
+            }
         }
     }
 
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
