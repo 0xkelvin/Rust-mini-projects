b0b28be4183ddddcd4a2e82a37dc1364f9503a23
Kelvin <quocvm1@gmail.com>
2022-09-03 16:38:29 +0800

    [simpleCLI] refactor, make code testable by put stuff in function

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -19,54 +19,25 @@ struct Cli {
 
 
 fn main() {
-    // println!("Hello, world!");
-
-    // let pattern = std::env::args().nth(1).expect("no pattern given");
-    // let path = std::env::args().nth(2).expect("no path given");
-
-    // let args = Cli {
-    //     pattern: pattern,
-    //     path: std::path::PathBuf::from(path),
-    // };
-
     let args = Cli::parse();
-    //[improvement] use BufReader instead 
-    // let content = std::fs::read_to_string(args.path)
-    //     .expect("cound not read file");
-    // // how to know what are the menthods available in 'content'
-    // for line in content.lines() {
-    //     if line.contains(&args.pattern) {
-    //         println!("{}", line);
-    //     }
-    // }
-    match read_lines(args.path) {
+
+    find_matches(args.path, &args.pattern);
+}
+
+fn find_matches(file_path: std::path::PathBuf, pattern: &str) {
+    match read_lines(file_path) {
         Ok(lines) => {
             for line in lines {
                 if let Ok(line) = line {
                     // todo!("need to improve nest 'if' statment");
-                    if line.contains(&args.pattern) {
+                    if line.contains(pattern) {
                         println!("{}", line);
                     }
                 }
             }
         },
-        Err(error) => {println!("{}", error);}
+        Err(error) => {eprintln!("Error:: {}", error);}
     }
-
-    // if let Ok(lines) = read_lines(args.path) {
-    //     // Consumes the iterator, returns an (Optional) String
-    //     for line in lines {
-    //         if let Ok(line) = line {
-    //             // todo!("need to improve nest 'if' statment");
-    //             if line.contains(&args.pattern) {
-    //                 println!("{}", line);
-    //             }
-    //         }
-    //     }
-    // } else {
-    //     println!("error");
-    // }
-
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
