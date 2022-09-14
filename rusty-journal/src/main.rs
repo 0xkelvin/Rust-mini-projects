a09dff7043c785c0613f220647967d08bcda98cc
Kelvin <quocvm1@gmail.com>
2022-09-14 20:37:53 +0800

    [todoCLI] add task modular

--- rusty-journal/src/main.rs
+++ rusty-journal/src/main.rs
@@ -1,7 +1,26 @@
-mod cli;
-
 use structopt::StructOpt;
+mod cli;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
 
 fn main() {
-    cli::CommandLineArgs::from_args();
+    // Get the command-line arguments
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // unpack the journal file
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    // perform the action
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+
+    }
+    .expect("Failed to perform action")
 }
